cd6cb489bdad49695fba446e56936702f9416d28
Dhruvwagh26 <114526284+Dhruvwagh26@users.noreply.github.com>
2024-02-06 11:38:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ if (amount < 10) {
 }
 
 console.log(`hey it's my first mode app!!!`);
+
+
+//For execution on terminal - install node.js and then write the given commands on the terminal to check and initialize node successfully.
+// for checking is node is installed successfuly >node --version
+// for initializing node application (here app.js)
+// > node app.js
